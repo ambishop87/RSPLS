24729a94c77f9afbdc9403d36e4359f95b0857d4
Ashley Bishop <cumminsgrl87@gmail.com>
2021-11-04 16:28:50 -0400

    updated details for methods in game class

---
+++ Player.js
@@ -0,0 +1,15 @@
+class Player {
+    constructor(player) {
+        this.player = ["playerOne", "playerTwo"];
+        this.score = 0;
+        this.selectedGesture = "";
+        this.gestures = ["Rock", "Paper", "Scissors", "Lizard","Spock"]
+    }
+
+    chooseGesture(); { 
+
+    }
+
+
+}
+module.exports = Player; 
---
+++ game.js
@@ -0,0 +1,37 @@
+class Game extends Player {
+    constructor(player) {
+        super(player)
+    // two human player options
+    this.playerOne;
+    this.playerTwo;
+}
+
+    runGame(){
+        this.displayRules();
+        this.chooseGameType();
+    }
+
+        displayRules(){
+            console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock! \n You may only have two players. \n Choose Single Player to play against computer. \n Choose Multiplayer to play against a friend.");
+        }
+
+            chooseGameType(){
+                console.log("Choose game type");
+                prompt( )
+            }
+
+            choosePlayerType(){
+                console.log("Choose Player Type");
+                this.singlePlayer;
+                this.multiplayer;
+                const prompt = require("prompt-sync")();
+               const input = prompt("Single Player or Multiplayer?");
+               console.log('You selected ${input');
+
+            }
+
+
+
+
+}
+module.exports = Game
