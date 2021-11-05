f444c9a96027e004a0c6340cece44ae75eb675ab
Ashley Bishop <cumminsgrl87@gmail.com>
2021-11-04 23:03:30 -0400

    Finished methods for ai class

--- ai.js
+++ ai.js
@@ -1,12 +1,15 @@
 class Ai {
-    constructor(ai) {
-        this.player = ["playerOne", "playerTwo"]:
+    constructor(Ai) {
+        this.player = ["playerOne", "playerTwo"];
         this.score = 0;
         this.selectedGesture = "";
         this.gestures = ["Rock", "Paper", "scissors", "Lizard", "Spock"]
     }
 
     chooseGesture(); {
+        console.log("The other player has selected a gesture");
         this.gestures = Math.floor(Math.random() * 3);
     }
 }
+
+module.exports = {Ai} 
